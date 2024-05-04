651c58145c6a72cc011ec813dd97ff84847c33b7
Matt <>
2024-05-04 13:28:37 +0100

    add list seats route

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import {
   userIdValidation,
   createEventValidation,
 } from './middleware/validation';
-import errorHandler from './middleware/errorHandler'
-import { createEventRoute } from './routes/eventSeats';
+import errorHandler from './middleware/errorHandler';
+import {
+  createEventRoute,
+  listSeatsRoute,
+} from './routes/eventSeats';
+import unknownRoute from './routes/unknownRoute';
 import redisClient from './redis';
 
 const app = express();
@@ -14,8 +18,10 @@ app.use(express.json());
 app.use(userIdValidation);
 
 app.post('/events', createEventValidation, createEventRoute);
+app.get('/events/:eventId/seats', listSeatsRoute);
+app.use(unknownRoute);
 
-app.use(errorHandler)
+app.use(errorHandler);
 const runningApp = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- src/routes/eventSeats.ts
+++ src/routes/eventSeats.ts
@@ -1,12 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import * as redis from '../redis/eventSeats';
+import { listFreeSeats } from '../lib/eventSeats';
 
 export async function createEventRoute(req: Request, res: Response, next: NextFunction) {
   try {
-    const { NumSeats } = req.body;
-    const { eventId, seats } = await redis.createEvent(NumSeats);
+    const { numSeats } = req.body;
+    const { eventId, seats } = await redis.createEvent(numSeats);
     res.status(200).json({
       eventId,
+      seats,
+      ok: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function listSeatsRoute(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { eventId } = req.params;
+    const seats = await listFreeSeats(eventId);
+    res.status(200).json({
       seats,
       ok: true,
     });
